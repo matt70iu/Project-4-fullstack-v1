97deab32fc3d2a501a05c6e81280479dd9bcf63c
matt70iu <mattadams60@gmail.com>
2022-08-09 13:14:13 +0000

    create basic templates, link in views and update settings

--- studyzone/settings.py
+++ studyzone/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'studyzone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
